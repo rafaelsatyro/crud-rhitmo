6b2d2e4d311e0dcbfe602b2df876617d826ec95d
TheSatyro <rafael.satyro@hotmail.com>
2022-11-03 10:59:08 -0300

    'first-commit-crud-rhitmo'

---
+++ src/app/pessoa/listar-pessoa/listar-pessoa.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+import { PessoaService } from '../services/pessoa.service';
+
+@Component({
+  selector: 'app-listar-pessoa',
+  templateUrl: './listar-pessoa.component.html',
+  styleUrls: ['./listar-pessoa.component.css']
+})
+export class ListarPessoaComponent implements OnInit {
+  pessoas : Pessoa [] = [];
+  constructor(private pessoaService: PessoaService) { }
+
+
+  ngOnInit(): void {
+    this.pessoas = this.listarTodos();
+  }
+ 
+  listarTodos(): Pessoa [] {
+    return this.pessoaService.listarTodos();
+  }
+
+  remover($event: any, pessoa: Pessoa): void {
+    $event.preventDefault();
+    if (confirm('Deseja realmente remover esta pessoa?')){
+      this.pessoaService.remover(pessoa.id!);
+      this.pessoas = this.listarTodos();
+    }
+  }
+  
+}
---
+++ src/app/shared/models/pessoa.model.ts
@@ -0,0 +1,12 @@
+export class Pessoa {
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public email?: string,
+        public cpf?: string,
+        public endereco?: string,
+        public estado?: string,
+        public cep?: number,
+        public cidade?: string,
+    ){}
+}
